36c622319f38bfefaa448ffae2fed078dcde150a
David Breskvar <dbreskvar@gmail.com>
2016-10-20 00:44:32 +0200

    visual tweeks

--- ToshlTest/app/src/main/java/dbug/toshltest/activities/AddEntryActivity.java
+++ ToshlTest/app/src/main/java/dbug/toshltest/activities/AddEntryActivity.java
@@ -1,13 +1,19 @@
 package dbug.toshltest.activities;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 import dbug.toshltest.R;
 import dbug.toshltest.helpers.CategoryHelper;
@@ -59,13 +65,36 @@ public class AddEntryActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        entryDateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddEntryActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        entryDateText.setText(String.format(Locale.getDefault(), "%d-%d-%d", year, (month + 1), day));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
     }
 
     private void createEntry() {
         Entry entry = new Entry();
         String description = entryDescriptionEdit.getText().toString();
         if (description.length() > 0) entry.setDesc(description);
-        entry.setAmount(Double.parseDouble(entryAmountEdit.getText().toString()));
+        try {
+            entry.setAmount(Double.parseDouble(entryAmountEdit.getText().toString()));
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, getString(R.string.amount_error), Toast.LENGTH_SHORT).show();
+            return;
+        }
         entry.setDate(entryDateText.toString());
 
         Currency currency = new Currency();
@@ -77,7 +106,7 @@ public class AddEntryActivity extends AppCompatActivity {
         entry.setCategory(entryCategoryEdit.getText().toString());
         entry.setDate("2016-10-19");
 
-        if (false) { //If tags are added -> which is currentyl not possible
+        if (false) { //If tags are added -> which is currently not possible
             TagHelper.getTagIds(this, entry, "Create");
         } else {
             CategoryHelper.findCategoryId(this, entry, "Create");
